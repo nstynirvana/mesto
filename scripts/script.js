a9a3381409fb6a5f2f74003ea9c37e60092933c5
Anastasia Savinova <nsty.nirvana@gmail.com>
2022-06-30 21:00:12 +0300

    feat:добавить hover для кнопок

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+const popup = document.querySelector('.popup');
+const openPopupButton = document.querySelector('.profile__edit-button');
+const closePopupButton = document.querySelector('.popup__close-button');
+
+function openPopupButton(open) {
+    // тело функции
+    if {
+        // esli najat profile__edit-button, to 
+    }
+  } 
+
+
+const username = popupContent.querySelector('.popup__name');
+const aboutname = popupContent.querySelector('.popup__job');
+
+// zdes doljno bit ne name function a ee znachenie - value
+
+function username(value ili popup__name) {
+    // тело функции
+    // eto znachenie dolchno go v pole block profile - profile__title
+    if {
+        a cho zdes
+    }
+    // vozvrashaemoe znachenie
+    let name = value ili popup__name;
+    return = name;
+  } 
+
+function aboutname(value ili popup__job) {
+    // тело функции
+    // eto znachenie dolchno go v pole block profile - profile__subtitle
+    if {
+
+    }
+    // vozvrashaemoe znachenie
+    let job = value ili popup__name;
+    return = job;
+  } 
