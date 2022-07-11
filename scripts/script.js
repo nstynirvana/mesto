cd5d4b6b4f5e82f233893a5e5a981d72b09b4785
Anastasia Savinova <nsty.nirvana@gmail.com>
2022-07-11 17:40:35 +0300

    feat:добавить кнопку удаления, реализовать открытие разных попапов при клике на разные кнопки

--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,32 @@ const jobInput = formElement.querySelector('.popup__text_type_job');
 const title = document.querySelector('.profile__title');
 const subtitle = document.querySelector('.profile__subtitle');
 
-function openPopup() {
-  popup.classList.add('popup_opened');
+const titleElement = document.querySelector('.element__title');
+const linkImage = document.querySelector('.element__image');
+const nameInputElement = formElement.querySelector('.popup__text_type_title');
+const linkInputElement = document.querySelector('.popup__text_type_image');
+
+const addPopupButton = document.querySelector('.profile__add-button');
+const popupAdd = document.querySelector('.popup_add');
+const popupEdit = document.querySelector('.popup_edit');
+const deleteCardButton = document.querySelector('.element__delete-button');
+const openImageBig = document.querySelector('.popup_visual');
+
+// button like
+const likeElementCard = document.querySelectorAll('.element__like-button');
+
+function likeCard() {
+  likeElementCard.classList.toggle('element__like-button_active');
+}
+
+function openPopupEdit() {
+  popupEdit.classList.add('popup_opened');
   nameInput.value = title.textContent;
   jobInput.value = subtitle.textContent;
+}
+
+function openPopupAdd() {
+  popupAdd.classList.add('popup_opened');
 }
 
 function closePopup() {
@@ -24,9 +46,21 @@ function formSubmitHandler(evt) {
   title.textContent = nameInput.value;
   subtitle.textContent = jobInput.value;
 
-  closePopup();
+  closePopup(popupEdit);
 }
 
+// function formSubmitHandler(evt) {
+//   evt.preventDefault();
+  
+//   titleElement.textContent = nameInputElement.value;
+//   linkImage.textContent = linkInputElement.value;
+
+//   closePopup(popupAdd);
+// }
+
 formElement.addEventListener('submit', formSubmitHandler);
-editPopupButton.addEventListener('click', openPopup);
+editPopupButton.addEventListener('click', openPopupEdit);
 closePopupButton.addEventListener('click', closePopup);
+addPopupButton.addEventListener('click', openPopupAdd);
+likeElementCard.addEventListener('click', likeCard);
+
