5c043ddd930a5b5bef647180e5982781d61a068c
Anastasia Savinova <nsty.nirvana@gmail.com>
2022-07-16 14:47:26 +0300

    fix: исправить функцию добавления новой карточки, лайка

--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,44 @@
-const popups = document.querySelectorAll('.popup')
+const popups = document.querySelectorAll('.popup'); // Список всех попапов
+const popupEdit = document.querySelector('.popup_edit');
+const popupAdd = document.querySelector('.popup_add');
+const popupVisual = document.querySelector('.popup_visual');
 
 const submitPopupButton = document.querySelector('.popup__submit-button');
 
-const formElements = document.querySelectorAll('.form'); // Список всех форм на странице
+const formElements = document.querySelectorAll('.form');// Список всех форм на странице
 const userEditForm = document.querySelector('#userEditForm');
 const cardCreateForm = document.querySelector('#cardCreateForm');
 
 const deleteCardButton = document.querySelector('.element__delete-button');
-const openImageBig = document.querySelector('.popup_visual');
 
-// button like
-const likeButtons = document.querySelectorAll('.element__like-button');
-
-likeButtons.forEach(btn => {
-  btn.addEventListener('click', likeCard);
-})
-
-function likeCard(event) {
-  const myLikeBtn = event.target;
-  myLikeBtn.classList.toggle('element__like-button_active');
-}
-
-// openPopupEdit
-
-function openPopup() {
-  popups.classList.add("popup_opened");
-}
-
+// openPopup
 const editPopupButton = document.querySelector('.profile__edit-button');
-const popupEdit = document.querySelector('.popup_edit');
+const addPopupButton = document.querySelector('.profile__add-button');
 
-const nameInput = formElement.querySelector('.popup__text_type_name');
-const jobInput = formElement.querySelector('.popup__text_type_job');
+// function openPopup(popup) {
+//   popup.classList.add("popup_opened");
+// }
+
+const nameInput = document.querySelector('.popup__text_type_name');
+const jobInput = document.querySelector('.popup__text_type_job');
+
 const title = document.querySelector('.profile__title');
 const subtitle = document.querySelector('.profile__subtitle');
 
 function openPopupEdit() {
   popupEdit.classList.add('popup_opened');
-  console.log(popupEdit)
   nameInput.value = title.textContent;
   jobInput.value = subtitle.textContent;
 }
 
 editPopupButton.addEventListener('click', openPopupEdit);
 
-// openPopupAdd
-const addPopupButton = document.querySelector('.profile__add-button');
-const popupAdd = document.querySelector('.popup_add');
+////////////////////////////////////////////////////////////////
 
 const cardTitle = document.querySelector('.element__title');
 const cardLink = document.querySelector('.element__image');
-// const nameInputElement = formElement.querySelector('.popup__text_type_title');
-// const linkInputElement = document.querySelector('.popup__text_type_image');
+const nameInputElement = document.querySelector('.popup__text_type_title');
+const linkInputElement = document.querySelector('.popup__text_type_image');
 
 function openPopupAdd() {
   popupAdd.classList.add('popup_opened');
@@ -63,22 +50,22 @@ addPopupButton.addEventListener('click', openPopupAdd);
 
 // closePopup
 
-function closePopup() {
-  popups.classList.remove('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-const closePopupButton = popups.querySelector('.popup__close-button');
+const closePopupButton = document.querySelector('.popup__close-button');
 
-popups.forEach(popup) => {
+popups.forEach(popup => {
   popup.addEventListener('click', (evt) => {
     if (evt.target.classList.contains('popup__close-button')) {
-      closePopup(popup)
+      closePopup(popup);
     }
-  })
-}
+  });
+});
 
 // obrabotchik first popup
-function formSubmitHandler(evt) {
+function formSubmitHandlerEdit(evt) {
   evt.preventDefault();
 
   title.textContent = nameInput.value;
@@ -87,7 +74,24 @@ function formSubmitHandler(evt) {
   closePopup(popupEdit);
 }
 
-formElements.addEventListener('submit', formSubmitHandler);
+userEditForm.addEventListener('submit', formSubmitHandlerEdit);
+
+// obrabotchik second popup
+function formSubmitHandlerAdd(evt) {
+  evt.preventDefault();
+  
+  const parameters = {
+    name: cardTitle.value,
+    link: cardLink.value
+  };
+
+  renderCard(parameters);
+  render();
+
+  closePopup(popupAdd);
+}
+
+cardCreateForm.addEventListener('submit', formSubmitHandlerAdd);
 
 // create New Card
 
@@ -137,8 +141,20 @@ function renderCard({ name, link }) {
     .querySelector(".element")
     .cloneNode(true);
   cardsElement.querySelector(".element__title").textContent = name;
-  cardsElement.querySelector(".element__image").src = link;
+  cardsElement.querySelector(".element__image").setAttribute('src', link);
 
-  cardsContainer.prepend(placeElement);
+  cardsContainer.prepend(cardsElement);
 }
 render();
+
+// button like
+const likeButtons = document.querySelectorAll('.element__like-button');
+
+likeButtons.forEach(btn => {
+  btn.addEventListener('click', likeCard);
+})
+
+function likeCard(event) {
+  const myLikeBtn = event.target;
+  myLikeBtn.classList.toggle('element__like-button_active');
+}
