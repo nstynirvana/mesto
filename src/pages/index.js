fa32eb565634d2d9376abc61c6fc2de27cc2e828
Anastasia Savinova <nsty.nirvana@gmail.com>
2022-09-03 11:06:01 +0300

    feat:собрать проект с помощью webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
-import FormValidator from "./FormValidator.js";
-import Card from "./Card.js";
-import Section from "./Section.js";
-import PopupWithImage from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
+import FormValidator from "../scripts/FormValidator.js";
+import Card from "../scripts/Card.js";
+import Section from "../scripts/Section.js";
+import PopupWithImage from "../scripts/PopupWithImage.js";
+import PopupWithForm from "../scripts/PopupWithForm.js";
+import UserInfo from "../scripts/UserInfo.js";
 import {
   elementsSelector,
   profileSelectors,
   initialCardsData,
   validationSelectors,
   formSelectors
-} from "./constants.js";
+} from "../constants/constants.js";
+import './index.css';
 
 // Попапы
 const cardDetailPopupImage = document.querySelector(".popup-visual__image");
